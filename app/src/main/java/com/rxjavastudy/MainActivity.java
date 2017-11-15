3cd54241f8798d097a942f37d75f443838390d1b
lvzhendong <lvzhendong1993@gmail.com>
2017-11-15 17:08:22 +0800

    一些操作符的使用

--- app/src/main/java/com/rxjavastudy/MainActivity.java
+++ app/src/main/java/com/rxjavastudy/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rxjavastudy;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.orhanobut.logger.Logger;
@@ -9,8 +11,20 @@ import com.rxjavastudy.api.API;
 import com.rxjavastudy.bean.response.Branch;
 import com.rxjavastudy.bean.response.UserFollower;
 
-import java.util.List;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -22,15 +36,20 @@ import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        listUserFollowers();
-
+//        listUserFollowers();
+//        countDownTimer();
+        initFlow();
     }
 
     void listUserFollowers() {
@@ -63,7 +82,7 @@ public class MainActivity extends Activity {
 
                     @Override
                     public void onNext(@NonNull List<Branch> branches) {
-                        Logger.i(branches.get(0).getName()+"");
+                        Logger.i(branches.get(0).getName() + "");
                     }
 
                     @Override
@@ -79,4 +98,76 @@ public class MainActivity extends Activity {
 
     }
 
+    @OnClick(R.id.tv)
+    public void onViewClicked() {
+    }
+
+    private void countDownTimer() {
+        final int timerLong = 10;
+        Observable.interval(1, TimeUnit.SECONDS)
+                .take(timerLong + 1)
+                .map(new Function<Long, Long>() {
+                    @Override
+                    public Long apply(Long aLong) throws Exception {
+                        return timerLong - aLong;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Logger.i("onSubscribe");
+                        Toast.makeText(MainActivity.this, "计时开始", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Logger.i(aLong + "");
+                        mTv.setText(aLong + "");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(MainActivity.this, "计时出错", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Toast.makeText(MainActivity.this, "倒计时结束", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void initFlow() {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                e.onNext(0);
+                e.onNext(1);
+                e.onNext(2);
+                e.onComplete();
+            }
+        }, BackpressureStrategy.ERROR)
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(2);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Logger.i(integer+"");
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Logger.i("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Logger.i("onComplete");
+                    }
+                });
+    }
 }
